8165f6c21fe9de0bdab95af552130053ab3a7e28
alonso <alonso.m144@gmail.com>
2019-11-06 21:42:37 -0500

    Add angularFire, firebase configuration for enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDhZ2YmCMk4mbeYgUrPJsLFF4n-GHWiPwU',
+    authDomain: 'evangelion-5eae1.firebaseapp.com',
+    databaseURL: 'https://evangelion-5eae1.firebaseio.com',
+    projectId: 'evangelion-5eae1',
+    storageBucket: 'evangelion-5eae1.appspot.com',
+    messagingSenderId: '823105675401',
+    appId: '1:823105675401:web:c4285a0189e3f872b96658'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDhZ2YmCMk4mbeYgUrPJsLFF4n-GHWiPwU',
+    authDomain: 'evangelion-5eae1.firebaseapp.com',
+    databaseURL: 'https://evangelion-5eae1.firebaseio.com',
+    projectId: 'evangelion-5eae1',
+    storageBucket: 'evangelion-5eae1.appspot.com',
+    messagingSenderId: '823105675401',
+    appId: '1:823105675401:web:c4285a0189e3f872b96658'
+  }
 };
 
 /*
